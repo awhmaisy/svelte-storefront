1be57d999a76ff3a36518c1e663b9fbd2db512e7
mei higashi <mei@meihigashi.com>
2024-08-01 19:38:13 -0700

    tf

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+    import { products, getProducts } from '../../store';
+    export async function load(ctx) {
+        await getProducts();
+        return { props: { products } };
+    }
+</script>
+
+<script>
+export let products;
+</script>
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+<section>
+<h2>
+    {#each $products as product}
+        <p>{product.node.title} </p>
+    {/each}
+</h2>
+</section>
